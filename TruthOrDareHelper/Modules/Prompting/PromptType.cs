660e0f6ed132f1ac759942b3c2bcc05e4fc6426f
Óscar Míguez Alonso <omiguezalonso@gmail.com>
2024-09-26 12:22:33 +0200

    Added skeleton for prompter

---
+++ TruthOrDareHelper/Modules/Prompting/PromptType.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruthOrDareHelper.Modules.Prompting
+{
+    public enum PromptType
+    {
+        TruthSFW,
+        TruthNSFW,
+        DareSFW,
+        DareNSFW,
+    }
+}
---
+++ TruthOrDareHelper/Modules/Prompting/Prompter.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruthOrDareHelper.Modules.Prompting
+{
+    public class Prompter
+    {
+        public string GetPrompt(PromptType promptType)
+        {
+            return $"TODO: This would give a random prompt of type {promptType}";
+        }
+    }
+}
