9f843cb650364e4951d3a1798d279c356398b5fe
Óscar Míguez Alonso <omiguezalonso@gmail.com>
2024-11-07 18:35:19 +0100

    Added SaveManager

---
+++ DalamudBasics/SaveGames/ISaveManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DalamudBasics.SaveGames
+{
+    public interface ISaveManager<T> where T : new()
+    {
+        DateTime? LastTimeSaved { get; }
+
+        T? LoadSave();
+        void WriteSave(T gameState);
+    }
+}
