175ad8ed39d948f4f58615e0a9e0a7cf3016f2a5
Óscar Míguez Alonso <omiguezalonso@gmail.com>
2024-11-26 21:59:06 +0100

    Codemaid pass.

--- DalamudBasics/Chat/Listener/ChatChannelSets.cs
+++ DalamudBasics/Chat/Listener/ChatChannelSets.cs
@@ -1,19 +1,33 @@
 using Dalamud.Game.Text;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DalamudBasics.Chat.Listener
 {
     public static class ChatChannelSets
     {
         public static XivChatType[] CommonChannels = [XivChatType.Say, XivChatType.Shout, XivChatType.Yell, XivChatType.Party, XivChatType.Alliance];
-        public static XivChatType[] CommonChannelsAndLinkshells = [XivChatType.Say, XivChatType.Shout, XivChatType.Yell, XivChatType.Party, XivChatType.Alliance,
-            XivChatType.CrossLinkShell1, XivChatType.CrossLinkShell2, XivChatType.CrossLinkShell3, XivChatType.CrossLinkShell4, XivChatType.CrossLinkShell5,
-            XivChatType.CrossLinkShell6, XivChatType.CrossLinkShell7, XivChatType.CrossLinkShell8,
-            XivChatType.Ls1, XivChatType.Ls2, XivChatType.Ls3, XivChatType.Ls4, XivChatType.Ls5, XivChatType.Ls6, XivChatType.Ls7, XivChatType.Ls8];
+
+        public static XivChatType[] CommonChannelsAndLinkshells = [XivChatType.Say,
+            XivChatType.Shout,
+            XivChatType.Yell,
+            XivChatType.Party,
+            XivChatType.Alliance,
+            XivChatType.CrossLinkShell1,
+            XivChatType.CrossLinkShell2,
+            XivChatType.CrossLinkShell3,
+            XivChatType.CrossLinkShell4,
+            XivChatType.CrossLinkShell5,
+            XivChatType.CrossLinkShell6,
+            XivChatType.CrossLinkShell7,
+            XivChatType.CrossLinkShell8,
+            XivChatType.Ls1,
+            XivChatType.Ls2,
+            XivChatType.Ls3,
+            XivChatType.Ls4,
+            XivChatType.Ls5,
+            XivChatType.Ls6,
+            XivChatType.Ls7,
+            XivChatType.Ls8];
+
         public static XivChatType[] PartyFinderchannels = [XivChatType.Party, XivChatType.Alliance];
     }
 }
